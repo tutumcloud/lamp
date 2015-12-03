7060f26ddd3b944fa2d5a9d8c096c4d8284eeecd
Hiram Chirino <hiram@hiramchirino.com>
2015-12-03 08:19:01 -0500

    Support setting up the mysql DB with some initial data.

--- create_mysql_admin_user.sh
+++ create_mysql_admin_user.sh
@@ -17,6 +17,10 @@ echo "=> Creating MySQL admin user with ${_word} password"
 mysql -uroot -e "CREATE USER 'admin'@'%' IDENTIFIED BY '$PASS'"
 mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' WITH GRANT OPTION"
 
+# You can create a /mysql-setup.sh file to intialized the DB
+if [ -f /mysql-setup.sh ] ; then
+  . /mysql-setup.sh
+fi
 
 echo "=> Done!"
 
